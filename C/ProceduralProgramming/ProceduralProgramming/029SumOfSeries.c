59dcfe5c524798b20b8c066aa98061b0318725ee
Florin Asavei <asaveiflorin@gmail.com>
2017-01-11 20:02:50 +0200

    fixed my git

--- C/ProceduralProgramming/ProceduralProgramming/029SumOfSeries.c
+++ C/ProceduralProgramming/ProceduralProgramming/029SumOfSeries.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
 void main(){
+	float x, y, t;
+	int k;
+	scanf_s("%f", &x);
+	y = 0;
+	t = 1; k = 1;
 
+	while (t > 0){
+		y = y + t;
+		t = t*x/k;
+		k++;
+	}
+	printf("%15.6f%15.6f\n", y, exp(x));
+	getchar();
 
 }
