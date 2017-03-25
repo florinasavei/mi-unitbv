b1a7da0e2374a86a0257372a0f78d92ccec0e943
Florin Asavei <asaveiflorin@gmail.com>
2017-03-25 09:20:18 +0200

    renamed files so that they start with 0

---
+++ python/calculus/lab/20170325/05.py
@@ -0,0 +1,12 @@
+#La banca se depun 100 Euro cu doamnda de 5% pe an.
+#In momentul in care se atinge suma de 200 Euro banii vor fi retrasi.
+#dupa cati ani este posibil acest lucru?
+bani=100
+dobanda=1.05
+ani=0
+while bani<200:
+    bani=bani*dobanda
+    ani=ani+1
+print('Sunt necesari ', ani, 'ani pentru a ajunge la ',bani,' euro')
+
+
