15556a5570555ddccf715e563709532e67d6c3c9
Florin Asavei <asaveiflorin@gmail.com>
2017-03-25 12:01:30 +0200

    refactored

--- python/calculus/lab/20170325/Algos/matrixCholescki.py
+++ python/calculus/lab/20170325/Algos/matrixCholescki.py
@@ -1,5 +1,5 @@
 from numpy import matrix, dot
-from math import sqrt
+
 from algoFunctions import cholescki, rezCholescki
 a = matrix([[1.00,2.00,1.0],[2.0,5.00,2.0],[1.0,2.0,3.0]])
 b = matrix([[5.0],[11.0],[7.0]])
