e7d94e6b515e5ca683560d953efaae535f44cd9c
Florin Asavei <asaveiflorin@gmail.com>
2017-03-25 11:10:16 +0200

    refactored

--- python/calculus/lab/20170325/Algos/algoFunctions.py
+++ python/calculus/lab/20170325/Algos/algoFunctions.py
@@ -16,7 +16,7 @@ def rezgauss(a,b):
         x[k]=(b[k]-dot(a[k,k+1:n], x[k+1:n]))/a[k,k]
     return x
 
-#descompunere LU doolitle
+#descompunere LU doolitle pentru metoda doolittle
 def LUdoolittle(a):
     n=len(a)
     for k in range(0,n-1):
@@ -28,17 +28,14 @@ def LUdoolittle(a):
                 a[i,k]=l
     return a
 
-#metodata de eliminare doolitle
-def rezdoolite(a,b):
+#metodata de eliminare doolittle
+def rezdoolitte(a,b):
+    a=LUdoolittle(a)
     n=len(b)
     x=zeros(n)
-    for k in range(0,n-1):
-        for i in range(k+1,n):
-            if a[i,k] !=0.0:
-                l = a[i,k]/a[k,k]
-                a[i,k]=a[i,k]-l*a[k,k]
-                a[i,k+1:n]=a[i,k+1:n]-l*a[k,k+1:n]
-                b[i]=b[i]-l*b[k]
+    y=b
+    for k in range(1,n):
+        y[k]=b[k]-dot(a[k,0:k],y[0:k])
     for k in range(n-1,-1,-1):
         x[k]=(b[k]-dot(a[k,k+1:n], x[k+1:n]))/a[k,k]
     return x
