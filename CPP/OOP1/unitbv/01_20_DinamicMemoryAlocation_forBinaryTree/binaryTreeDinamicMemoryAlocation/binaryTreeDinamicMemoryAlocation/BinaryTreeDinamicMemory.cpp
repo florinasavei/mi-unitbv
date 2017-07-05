9874f73b0136ccb6ded80ba0cd892d7d0b250ab2
Asavei Florin <asaveiflorin@gmail.com>
2017-07-05 21:40:45 +0300

    some more c++ stuff

---
+++ CPP/OOP1/unitbv/01_20_DinamicMemoryAlocation_forBinaryTree/binaryTreeDinamicMemoryAlocation/binaryTreeDinamicMemoryAlocation/BinaryTreeDinamicMemory.cpp
@@ -0,0 +1,12 @@
+//This program dinamically alocates and frees memory for a binary tree
+
+struct TarbBin
+{
+	char info[20];
+	struct *TarbBin, *ls, *ld;
+}*rad;
+
+//..
+
+if(!(rad = new struct TArbBin))
+	
