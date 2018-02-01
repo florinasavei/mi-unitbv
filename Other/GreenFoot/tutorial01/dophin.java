500ed0c8f427d672d89ee67c6af7241ef2616573
Asavei Florin <asaveiflorin@gmail.com>
2018-02-01 22:08:37 +0200

    greenFootExample

---
+++ Other/GreenFoot/tutorial01/dophin.java
@@ -0,0 +1,35 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class dophin here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class dophin extends Actor
+{
+    /**
+     * Act - do whatever the dophin wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+        if(Greenfoot.isKeyDown("left"))
+        {
+            turn(-5);
+        }  
+        if(Greenfoot.isKeyDown("right"))
+        {
+            turn(5);
+        }
+        if(Greenfoot.isKeyDown("up"))
+        {
+            move(5);
+        }
+        if(Greenfoot.isKeyDown("down"))
+        {
+            move(-5);
+        }
+    }
+}
