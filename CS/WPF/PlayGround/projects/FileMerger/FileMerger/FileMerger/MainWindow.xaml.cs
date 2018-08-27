2fd9f84c5f893185c1ce4330dfc6fd68c0371e1d
Florin Asavei <asaveiflorin@gmail.com>
2018-08-27 21:58:08 +0300

    working on file merger

---
+++ CS/WPF/PlayGround/projects/FileMerger/FileMerger/FileMerger/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FileMerger
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSourcePathSelector_OnClick(object sender, RoutedEventArgs e)
+        {
+            var f = new FolderBrowserDialog
+            {
+                Description = @"Please the a folder containing the files you want to join:"
+            };
+
+            var result = f.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK)
+                rootSourcePath.Text = f.SelectedPath + @"\";
+        }
+
+       
+
+        private void BtnJoinFiles(object sender, RoutedEventArgs e)
+        {
+
+
+            List<string> allWordDocuments = Directory.GetFiles( rootSourcePath.Text, "*.docx", SearchOption.AllDirectories)
+                .Where(d => !MainWindow.isExcluded(d, rootSourcePath.Text))
+                .ToList();
+            string outputPath = rootSourcePath.Text + @"/_combined/Combined.docx";
+
+            bool folderExists = Directory.Exists(rootSourcePath.Text + @"/_combined");
+            if (!folderExists)
+                Directory.CreateDirectory(rootSourcePath.Text + @"/_combined");
+
+            allWordDocuments.Remove(outputPath);
+
+            FileMerger.MainWindow.Merge(allWordDocuments, outputPath, true); 
+
+        }
+
+        static bool isExcluded( string target, string rootDirectory)
+        {
+            var dirName = System.IO.Path.GetDirectoryName(target);
+            List<string> exludedDirList = new List<string>(new string[] { "_combined"});
+            return exludedDirList.Any(d => dirName.Equals(rootDirectory + d));
+        }
+
+    }
+}
---
+++ CS/WPF/PlayGround/projects/FileMerger/FileMerger/FileMerger/MergeFiles.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace FileMerger
+{
+    public partial class MainWindow
+    {
+        public static void Merge(List<string>filesToMerge, string outputFilename, bool insertPageBreaks, string documentTemplate = "")
+        {
+
+            if (filesToMerge.Capacity == 0)
+            {
+                System.Windows.MessageBox.Show("No Files to be merged!");
+
+                return;
+            }
+
+            if (File.Exists(outputFilename))
+            {
+                FileInfo file = new FileInfo(outputFilename);
+                MainWindow.Rename(file,"Combined_" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".docx");
+            }
+
+            //object defaultTemplate = documentTemplate; //not sure what this does
+            object missing = System.Type.Missing;
+            object pageBreak = Word.WdBreakType.wdSectionBreakNextPage;
+            object outputFile = outputFilename;
+
+            // Create a new Word application
+            Word._Application wordApplication = new Word.Application();
+
+            try
+            {
+                // Create a new file based on our template
+                Word.Document wordDocument = wordApplication.Documents.Add(
+                                              ref missing
+                                            , ref missing
+                                            , ref missing
+                                            , ref missing);
+
+                // Make a Word selection object.
+                Word.Selection selection = wordApplication.Selection;
+
+                //Count the number of documents to insert;
+                int documentCount = filesToMerge.Capacity;
+
+                //A counter that signals that we shoudn't insert a page break at the end of document.
+                int breakStop = 0;
+
+                // Loop thru each of the Word documents
+                foreach (string file in filesToMerge)
+                {
+                    breakStop++;
+                    // Insert the files to our template
+                    selection.InsertFile(
+                                                file
+                                            , ref missing
+                                            , ref missing
+                                            , ref missing
+                                            , ref missing);
+
+                    //Do we want page breaks added after each documents?
+                    if (insertPageBreaks && breakStop != documentCount)
+                    {
+                        selection.InsertBreak(ref pageBreak);
+                    }
+
+                    selection.TypeText("some extra text");
+                }
+
+                // Save the document to it's output file.
+                wordDocument.SaveAs(
+                                ref outputFile
+                            , ref missing
+                            , ref missing
+                            , ref missing
+                            , ref missing
+                            , ref missing
+                            , ref missing
+                            , ref missing
+                            , ref missing
+                            , ref missing
+                            , ref missing
+                            , ref missing
+                            , ref missing
+                            , ref missing
+                            , ref missing
+                            , ref missing);
+
+                // Clean up!
+                wordDocument = null;
+            }
+            catch (Exception ex)
+            {
+                //I didn't include a default error handler so i'm just throwing the error
+                throw ex;
+            }
+            finally
+            {
+                // Finally, Close our Word application
+                wordApplication.Quit(ref missing, ref missing, ref missing);
+            }
+        }
+
+        public static void Rename(FileInfo fileInfo, string newName)
+        {
+            fileInfo.MoveTo(fileInfo.Directory.FullName + "\\" + newName);
+        }
+    }
+}
