9852dc937a5aabb4d78fc8584e6978dcfd943508
Florin Asavei <asaveiflorin@gmail.com>
2019-03-10 12:18:16 +0200

    almost finished

--- CS/WPF/Labs/WPF-Labs-2019/Lab-2019-Mar-10/MainWindow.xaml.cs
+++ CS/WPF/Labs/WPF-Labs-2019/Lab-2019-Mar-10/MainWindow.xaml.cs
@@ -20,78 +20,111 @@ namespace Lab_2019_Mar_10
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        public string SelectedOption { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void RadioButton_Checked(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        public delegate void DoAction();
-
-        public DoAction _myAction { get; set; }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            _myAction();
-        }
 
         private void OptionsMenu_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             RadioButton selectedRadio = (System.Windows.Controls.RadioButton)OptionsMenu.SelectedItems[0];
-            HandleSelectionChanged(selectedRadio);
+            HandleSelectionChangedCommon(selectedRadio);
         }
 
-        private void HandleSelectionChanged(RadioButton selectedRadio)
+        private void HandleSelectionChangedCommon(RadioButton selectedRadio)
         {
-            string selectedRadioName = selectedRadio.Name;
-            switch (selectedRadioName)
+            ClearInputAndResult();
+
+            SelectedOption = selectedRadio.Name;
+            switch (SelectedOption)
             {
                 case "rbHello":
-                    PrepareInput(selectedRadioName);
-                    _myAction = SayHello;
+                    SelectRadio();
+                    PrepareInput();
                     break;
                 case "rbAverage":
-                    PrepareInput(selectedRadioName);
-                    _myAction = CalcualteAverage;
+                    SelectRadio();
+                    PrepareInput();
                     break;
                 case "rbCheckPalindrom":
-                    PrepareInput(selectedRadioName);
-                    _myAction = CheckPalindrom;
+                    SelectRadio();
+                    PrepareInput();
                     break;
             }
         }
-     
-        private void SayHello()
+
+        private void SelectRadio()
         {
-            lblInput.Text = "Enter your name";
-            lblResult.Text = "Hello " + txtInpt.Text + " !";
+            rbHello.IsChecked = false;
+            rbAverage.IsChecked = false;
+            rbCheckPalindrom.IsChecked = false;
+
+            switch (SelectedOption)
+            {
+                case "rbHello":
+                    rbHello.IsChecked = true;
+                    break;
+                case "rbAverage":
+                    rbAverage.IsChecked = true;
+                    break;
+                case "rbCheckPalindrom":
+                    rbCheckPalindrom.IsChecked = true;
+                    break;
+            }
         }
 
-        private void CalcualteAverage()
+        #region Action Reults
+
+        private string SayHello(string s)
         {
-            throw new NotImplementedException();
+            return "Hello, " + s + " !  your name has " + s.Length + " characters";
         }
 
-        private void CheckPalindrom()
+        private float Average(string s)
         {
-            throw new NotImplementedException();
+            List<int> listOfNumbers = GetNumbersFromString(s);
+
+            return (float)listOfNumbers.Average();
         }
 
-
-        private void PrepareInput(string selectedRadioName)
+        private List<int> GetNumbersFromString(string s)
         {
-            ClearInput();
-            switch (selectedRadioName)
+            try
+            {
+                return new List<int>(Array.ConvertAll(s.Split(' '), int.Parse));
+            }
+            catch (Exception e)
+            {
+                lblResult.Text = "Invalid numbers";
+                throw new Exception(e.Message);
+            }
+
+            return new List<int>();
+        }
+
+        private bool IsPalindrom(string s)
+        {
+            string myString = s.ToLower();
+            string reversedString = new string(myString.Reverse().ToArray());
+            return string.CompareOrdinal(myString, reversedString) == 0;
+        }
+
+        #endregion
+
+        private void PrepareInput()
+        {
+            ClearInputAndResult();
+            switch (SelectedOption)
             {
                 case "rbHello":
                     lblInput.Text = "Enter your name";
                     break;
                 case "rbAverage":
-                    lblInput.Text = "Enter some numbers (comma separated)";
+                    lblInput.Text = "Enter some numbers (space separated)";
                     break;
                 case "rbCheckPalindrom":
                     lblInput.Text = "Enter any text";
@@ -99,15 +132,90 @@ namespace Lab_2019_Mar_10
             }
         }
 
-        private void ClearInput()
+        private void ClearInputAndResult()
         {
-           
+            txtInpt.Text = "";
+            lblResult.Text = "";
+        }
+
+        private bool IsInputValid()
+        {
+            if (string.IsNullOrWhiteSpace(txtInpt.Text))
+            {
+                lblResult.Text = "Enter something in input first";
+                return false;
+            }
+
+            switch (SelectedOption)
+            {
+                case "rbHello":
+                    return true;
+                case "rbAverage":
+                    if (txtInpt.Text.Contains(","))
+                    {
+                        lblResult.Text = "Invalid numbers";
+                        return false;
+                    }
+                    if (!char.IsDigit(txtInpt.Text[0]))
+                    {
+                        lblResult.Text = "Invalid characters";
+                        return false;
+                    }
+                    return true;
+                    break;
+                case "rbCheckPalindrom":
+                    return true;
+            }
+
+            return false;
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            switch (SelectedOption)
+            {
+                case "rbHello":
+                    if (!IsInputValid())
+                    {
+                        return;
+                    }
+                    lblResult.Text = SayHello(txtInpt.Text).ToString();
+                    break;
+                case "rbAverage":
+                    if (!IsInputValid())
+                    {
+                        return;
+                    }
+                    lblResult.Text = "The average is: " + Average(txtInpt.Text);
+                    break;
+                case "rbCheckPalindrom":
+                    if (!IsInputValid())
+                    {
+                        return;
+                    }
+                    lblResult.Text = "\"" + txtInpt.Text + " \"" + (IsPalindrom(txtInpt.Text) ? " is a palindrom" : " is not a palindrom");
+                    break;
+            }
         }
 
         private void RbHello_OnClick(object sender, RoutedEventArgs e)
         {
-            RadioButton radioName = (System.Windows.Controls.RadioButton)sender;
-            HandleSelectionChanged(radioName);
+            RadioButton radioBtn = (System.Windows.Controls.RadioButton)sender;
+            SelectedOption = radioBtn.Name;
+            switch (SelectedOption)
+            {
+                case "rbHello":
+                    OptionsMenu.SelectedIndex = 0;
+                    break;
+                case "rbAverage":
+                    OptionsMenu.SelectedIndex = 1;
+                    break;
+                case "rbCheckPalindrom":
+                    OptionsMenu.SelectedIndex = 2;
+                    break;
+            }
+            HandleSelectionChangedCommon(radioBtn);
         }
     }
 }
