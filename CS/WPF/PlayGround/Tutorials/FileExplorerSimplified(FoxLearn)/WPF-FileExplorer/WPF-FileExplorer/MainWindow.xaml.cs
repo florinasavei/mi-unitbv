8c37ef0c2f42dc90bf5c29e44942b5e4764067ac
Florin Asavei <asaveiflorin@gmail.com>
2019-04-07 10:58:49 +0300

    simple WFP file browser

---
+++ CS/WPF/PlayGround/Tutorials/FileExplorerSimplified(FoxLearn)/WPF-FileExplorer/WPF-FileExplorer/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace WPF_FileExplorer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnOpen_Click(object sender, RoutedEventArgs e)
+        {
+            using (CommonOpenFileDialog cfd = new CommonOpenFileDialog(){IsFolderPicker = true})
+            {
+                if (cfd.ShowDialog() == CommonFileDialogResult.Ok)
+                {
+                    fileBrowser.Source = new Uri(cfd.FileName);
+                    txtPath.Text = cfd.FileName;
+                }
+            }
+        }
+
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            if (fileBrowser.CanGoBack)
+            {
+                fileBrowser.GoBack();
+            }
+        }
+
+        private void BtnForward_Click(object sender, RoutedEventArgs e)
+        {
+            if (fileBrowser.CanGoForward)
+            {
+                fileBrowser.GoForward();
+            }
+        }
+    }
+}
